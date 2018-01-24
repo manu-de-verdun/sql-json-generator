70d545ef092d540dbff2cc3b4e87bb9caf6566f1
emmanuel.pautet <emmanuel.pautet@gmail.com>
2018-01-24 12:40:40 -0300

    mocha

---
+++ test/pgsql.js
@@ -0,0 +1,44 @@
+var should = require('chai').should();
+var expect = require('chai').expect;
+
+var SQLGenerator = require('../index');
+
+describe('#pgSQL - SELECT', function () {
+
+    var sqlGenerator = new SQLGenerator( {pgSQL : true} );
+    var sqlParams;
+
+    it('query #01', function () {
+
+        sqlParams = {
+    $from: 'iab_cidadao',
+    $fields: [,
+        'nr_cartao_sus',
+        {
+            $inner: 'grl_pessoa',
+            $on: {
+                $parent: 'cd_cidadao',
+                $child: 'cd_pessoa'
+            },
+            $fields: [
+                'nm_pessoa'
+            ],
+            $where: [{
+                $field : 'nm_pessoa',
+                $like: '%EMMA%'
+            }]
+        }
+    ],
+    $where: [],
+    $order: [{
+        $field: 'nm_pessoa',
+        $table: 'grl_pessoa',
+    }]
+};
+
+        var expectedResult = 'SELECT iab_cidadao.nr_cartao_sus, grl_pessoa.nm_pessoa FROM iab_cidadao INNER JOIN grl_pessoa ON iab_cidadao.cd_cidadao = grl_pessoa.cd_pessoa WHERE grl_pessoa.nm_pessoa LIKE \'%EMMA%\' ORDER BY grl_pessoa.nm_pessoa';
+
+        sqlGenerator.select(sqlParams).should.equal(expectedResult);
+
+    });
+});
